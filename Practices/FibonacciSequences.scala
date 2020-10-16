7e71c345c5ff74f8d8182f2988422e256131e134
Jonathan Angeles Valadez <jonathanangeles67@gmail.com>
2020-10-15 20:51:36 -0700

    Second commit to FibonacciSequence

--- Practices/FibonacciSequences.scala
+++ Practices/FibonacciSequences.scala
@@ -1,3 +1,49 @@
+//ALGORITHM 1: DESCENDING TAIL RECURSION VERSION
+/*
+ANGELES VALADEZ JONATHAN
+This algorithm is a tail based algorithm and it is built for the evasion of the
+stack overflow error, using 3 variables, n which is the number you wish to know
+the fibonacci value for, and, 'a' and 'b', which are values that store intermediate
+values, basically they are accumulators, where 'a' starts with 0 and 'b' starts with 1.
+When n = 0, b will be printed with the fibonacci value.
+*/
+def fibonacciR(n:Int, a:Int, b:Int) : Int = {
+ if ( n == 0 )
+  b
+ else
+  fibonacciR( n - 1, b, a + b )
+}
+
+fibonacciR(5,0,1)
+fibonacciR(10,0,1)
+fibonacciR(20,0,1)
+
+//ALGORITHM 2: EXPLICIT FORMULA VERSION
+/*
+ANGELES VALADEZ JONATHAN
+This algorithm uses the explicit formula for the fibonacci series, this formula isn't as
+precise as when you use the conventional methods like cycles or recursion, at least that's
+the conclusion that I got to, given that since it uses a lot of decimals, it get go way off
+the mark or even closer to it, I tried using the formula in various ways(as you can see with the
+commented values), but the result isn't very precise.
+*/
+def fibonacciFormula(n:Double) : Double = {
+    var a: Double = 0
+    var b: Double = 0
+    if ( n < 2 )
+        b
+    else
+        //a = ((1 + (Math.sqrt(5)) ) /2)
+        a = 1.61
+        b = (( ( (Math.pow(a,n)) - (Math.pow((1-a),n)) ) / (Math.sqrt(5)) ))
+        //b = (( ( (Math.pow(1.61,n)) - (Math.pow(-0.61,n) )) / (1.61) ))
+        b
+}
+fibonacciFormula(5)
+fibonacciFormula(8)
+fibonacciFormula(10)
+
+
 // ALGORITHM 3: ITERATIVE VERSION
 
 def fibonacci(num:Int):Int={
